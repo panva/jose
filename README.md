b8709756f087d9968741c2cb1a50d4a79f1ee3be
Filip Skokan <panva.ip@gmail.com>
2018-11-03 12:30:32 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# WIP
+
+```
+$ nvm use stable
+$ node removeme/some.js
+```
+
+# ultimate-jose
+
+[![build][travis-image]][travis-url] [![codecov][codecov-image]][codecov-url]
+
+TODO:
+
+**Table of Contents**
+
+TODO:
+
+## Implemented specs & features
+
+TODO:
+
+Missing a feature? - If it wasn't already discussed before, [ask for it][suggest-feature].  
+Found a bug? - [report it][bug].
+
+<h2>Support</h2>
+
+[<img src="https://c5.patreon.com/external/logo/become_a_patron_button@2x.png" width="160" align="right">][support-patreon]
+If you or your business use ultimate-jose, please consider becoming a [Patron][support-patreon] so I can continue maintaining it and adding new features carefree. You may also donate one-time via [PayPal][support-paypal].
+[<img src="https://cdn.jsdelivr.net/gh/gregoiresgt/payment-icons@183140a5ff8f39b5a19d59ebeb2c77f03c3a24d3/Assets/Payment/PayPal/Paypal@2x.png" width="100" align="right">][support-paypal]
+
+## Usage
+
+TODO:
+
+
+[travis-image]: https://api.travis-ci.com/panva/ultimate-jose.svg?branch=master
+[travis-url]: https://travis-ci.com/panva/ultimate-jose
+[codecov-image]: https://img.shields.io/codecov/c/github/panva/ultimate-jose/master.svg
+[codecov-url]: https://codecov.io/gh/panva/ultimate-jose
+[suggest-feature]: https://github.com/panva/ultimate-jose/issues/new?template=feature-request.md
+[bug]: https://github.com/panva/ultimate-jose/issues/new?template=bug-report.md
+[support-patreon]: https://www.patreon.com/panva
+[support-paypal]: https://www.paypal.me/panva
---
+++ removeme/some.js
@@ -0,0 +1,50 @@
+/* eslint-disable */
+const { strict: assert } = require('assert')
+const { readFileSync } = require('fs')
+
+const { JWS, JWK: { importKey } } = require('..')
+
+const ecPrivate = importKey(readFileSync('./removeme/ec.key'))
+const rsaPrivate = importKey(readFileSync('./removeme/rsa.key'))
+const ecPub = importKey(readFileSync('./removeme/ec.pem'))
+const rsaPub = importKey(readFileSync('./removeme/rsa.pem'))
+const payload = { sub: '1234567890', name: 'John Doe', iat: 1516239022 }
+
+// compact ec
+;(() => {
+  const signed = JWS.sign(payload, ecPrivate, { alg: 'ES256' }, undefined)
+  console.log('compact ES256 signed', signed, '\n')
+  assert(JWS.verify(signed, ecPrivate), 'compact JWS verification with ecPrivate failed')
+  assert(JWS.verify(signed, ecPub), 'compact JWS verification with ecPub failed')
+})();
+
+// compact rsa
+(() => {
+  const signed = JWS.sign(payload, rsaPrivate, { alg: 'RS256' }, undefined)
+  console.log('compact RS256 signed', signed, '\n')
+  assert(JWS.verify(signed, rsaPrivate), 'compact JWS verification with rsaPrivate failed')
+  assert(JWS.verify(signed, rsaPub), 'compact JWS verification with rsaPub failed')
+})();
+
+// flattened
+(() => {
+  const jws = new JWS.Sign({ sub: 'Filip', name: 'Skokan' }, { typ: 'JWT' })
+  jws.recipient(ecPrivate, { alg: 'ES256' })
+  const signed = jws.sign('flattened')
+  console.log('flattened ES256 signed', signed, '\n')
+  assert(JWS.verify(signed, ecPrivate), 'flattened JWS verification with ecPrivate failed')
+  assert(JWS.verify(signed, ecPub), 'flattened JWS verification with ecPub failed')
+})();
+
+// general
+(() => {
+  const jws = new JWS.Sign({ sub: 'Filip', name: 'Skokan' }, { typ: 'JWT' })
+  jws.recipient(ecPrivate, { alg: 'ES256' })
+  jws.recipient(rsaPrivate, { alg: 'RS512' })
+  const signed = jws.sign('general')
+  console.log('general ES256 and RS512 signed', signed, '\n')
+  assert(JWS.verify(signed, ecPrivate), 'general JWS verification with ecPrivate failed')
+  assert(JWS.verify(signed, ecPub), 'general JWS verification with ecPub failed')
+  assert(JWS.verify(signed, rsaPrivate), 'general JWS verification with rsaPrivate failed')
+  assert(JWS.verify(signed, rsaPub), 'general JWS verification with rsaPub failed')
+})();
