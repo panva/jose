be3f4e4b7ee72ea246acd9224c8d01056f36d811
Filip Skokan <panva.ip@gmail.com>
2019-03-04 22:56:25 +0100

    fix: do not 'in' operator when importing keys as string

--- test/jwk/import.test.js
+++ test/jwk/import.test.js
@@ -36,6 +36,25 @@ test('parameters must be a plain object', t => {
   })
 })
 
+Object.entries(fixtures.PEM).forEach(([type, { private: priv, public: pub }]) => {
+  test(`${type} private can be imported as a string`, t => {
+    const k = importKey(priv.toString('ascii'))
+    t.true(k.private)
+  })
+  test(`${type} public can be imported as a string`, t => {
+    const k = importKey(pub.toString('ascii'))
+    t.true(k.public)
+  })
+  test(`${type} private can be imported as a buffer`, t => {
+    const k = importKey(priv)
+    t.true(k.private)
+  })
+  test(`${type} public can be imported as a buffer`, t => {
+    const k = importKey(pub)
+    t.true(k.public)
+  })
+})
+
 test('failed to import throws an error', t => {
   t.throws(() => {
     importKey({
@@ -43,4 +62,17 @@ test('failed to import throws an error', t => {
       format: 'der'
     })
   }, { instanceOf: errors.JWKImportFailed, code: 'ERR_JWK_IMPORT_FAILED' })
+})
+
+;[
+  `-----BEGIN PRIVATE KEY-----\nMC4CAQAwBQYDK2VwBCIEIHXLsXm1lsq5HtyqJwQyFmpfEluuf0KOqP6DqMgGxxDL\n-----END PRIVATE KEY-----`,
+  `-----BEGIN PUBLIC KEY-----\nMCowBQYDK2VwAyEAEXRYV3v5ucrHVR3mKqyPXxXqU34lASwc7Y7MoOvaqcs=\n-----END PUBLIC KEY-----`,
+  `-----BEGIN PRIVATE KEY-----\nMEcCAQAwBQYDK2VxBDsEObxytD95dGN3Hxk7kVk+Lig1rGYTRr3YdaHjRog++Sgk\nQD7KwKmxroBURtkE2N0JbQ3ctdrpGRB5DQ==\n-----END PRIVATE KEY-----`,
+  `-----BEGIN PUBLIC KEY-----\nMEMwBQYDK2VxAzoAIESY3jnpGdB5UVJDCznrv0vmBFIzgSMu+gafsbCX1rFtsJwR\nM6XUDQiEY7dk6rmm/Fktyawna5EA\n-----END PUBLIC KEY-----`
+].forEach((unsupported, i) => {
+  test.skip(`fails to import unsupported PEM ${i + 1}/4`, t => {
+    t.throws(() => {
+      importKey(unsupported)
+    }, { instanceOf: errors.JWKImportFailed, code: 'ERR_JWK_IMPORT_FAILED' })
+  })
 })
