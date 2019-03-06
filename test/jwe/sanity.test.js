3d8b3a5fe8d7ca3c6796e1d4c63d5d7415dafa1e
Filip Skokan <panva.ip@gmail.com>
2019-03-06 21:50:25 +0100

    test: re-enable lint, test with min node, remove BLNS

--- test/jwe/sanity.test.js
+++ test/jwe/sanity.test.js
@@ -1,5 +1,4 @@
 const test = require('ava')
-const BLNS = require('big-list-of-naughty-strings')
 
 const base64url = require('../../lib/help/base64url')
 const { JWKS, JWK: { generateSync }, JWE, errors } = require('../..')
@@ -166,16 +165,6 @@ test('aes_cbc_hmac_sha2 decrypt iv check (missing)', t => {
   }, { instanceOf: errors.JWEInvalid, code: 'ERR_JWE_INVALID', message: 'JWE malformed or invalid serialization' })
 })
 
-test('aes_cbc_hmac_sha2 decrypt iv check (BLNS)', t => {
-  const k = generateSync('oct', undefined, { alg: 'A128CBC-HS256' })
-  const encrypted = JWE.encrypt.flattened('foo', k, { alg: 'dir', enc: k.alg })
-  BLNS.forEach((naughty) => {
-    encrypted.iv = naughty
-    const err = t.throws(() => { JWE.decrypt(encrypted, k) })
-    t.true(['ERR_JWE_INVALID', 'ERR_JWE_DECRYPTION_FAILED'].includes(err.code))
-  })
-})
-
 test('aes_cbc_hmac_sha2 decrypt iv check (invalid length)', t => {
   const k = generateSync('oct', undefined, { alg: 'A128CBC-HS256' })
   const encrypted = JWE.encrypt.flattened('foo', k, { alg: 'dir', enc: k.alg })
@@ -192,16 +181,6 @@ test('aes_cbc_hmac_sha2 decrypt tag check (missing)', t => {
   t.throws(() => {
     JWE.decrypt(encrypted, k)
   }, { instanceOf: errors.JWEInvalid, code: 'ERR_JWE_INVALID', message: 'JWE malformed or invalid serialization' })
-})
-
-test('aes_cbc_hmac_sha2 decrypt tag check (BLNS)', t => {
-  const k = generateSync('oct', undefined, { alg: 'A128CBC-HS256' })
-  const encrypted = JWE.encrypt.flattened('foo', k, { alg: 'dir', enc: k.alg })
-  BLNS.forEach((naughty) => {
-    encrypted.tag = naughty
-    const err = t.throws(() => { JWE.decrypt(encrypted, k) })
-    t.true(['ERR_JWE_INVALID', 'ERR_JWE_DECRYPTION_FAILED'].includes(err.code))
-  })
 })
 
 test('aes_cbc_hmac_sha2 decrypt tag check (invalid length)', t => {
@@ -222,16 +201,6 @@ test('aes_gcm decrypt iv check (missing)', t => {
   }, { instanceOf: errors.JWEInvalid, code: 'ERR_JWE_INVALID', message: 'JWE malformed or invalid serialization' })
 })
 
-test('aes_gcm decrypt iv check (BLNS)', t => {
-  const k = generateSync('oct', 128, { alg: 'A128GCM' })
-  const encrypted = JWE.encrypt.flattened('foo', k, { alg: 'dir', enc: k.alg })
-  BLNS.forEach((naughty) => {
-    encrypted.iv = naughty
-    const err = t.throws(() => { JWE.decrypt(encrypted, k) })
-    t.true(['ERR_JWE_INVALID', 'ERR_JWE_DECRYPTION_FAILED'].includes(err.code))
-  })
-})
-
 test('aes_gcm decrypt iv check (invalid length)', t => {
   const k = generateSync('oct', 128, { alg: 'A128GCM' })
   const encrypted = JWE.encrypt.flattened('foo', k, { alg: 'dir', enc: k.alg })
@@ -248,16 +217,6 @@ test('aes_gcm decrypt tag check (missing)', t => {
   t.throws(() => {
     JWE.decrypt(encrypted, k)
   }, { instanceOf: errors.JWEInvalid, code: 'ERR_JWE_INVALID', message: 'JWE malformed or invalid serialization' })
-})
-
-test('aes_gcm decrypt tag check (BLNS)', t => {
-  const k = generateSync('oct', 128, { alg: 'A128GCM' })
-  const encrypted = JWE.encrypt.flattened('foo', k, { alg: 'dir', enc: k.alg })
-  BLNS.forEach((naughty) => {
-    encrypted.tag = naughty
-    const err = t.throws(() => { JWE.decrypt(encrypted, k) })
-    t.true(['ERR_JWE_INVALID', 'ERR_JWE_DECRYPTION_FAILED'].includes(err.code))
-  })
 })
 
 test('aes_gcm decrypt tag check (invalid length)', t => {
