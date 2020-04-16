da20019df8ce16ea1f97d08c89d31e0a17c58907
Sam Curren <telegramsam@gmail.com>
2020-04-16 14:15:54 -0600

    chore: updated key types and passing tests
    Signed-off-by: Sam Curren <telegramsam@gmail.com>

--- test/jwm/examples.test.js
+++ test/jwm/examples.test.js
@@ -14,17 +14,9 @@ test('example-encrypted-jwm-single-recipient', t => {
         expiry : 1516239022,
         time_stamp : 1516269022,
         body : { message: "Hello world!" }
-    };
+    }
 
-    //Get recipient key
-
-    const recipient_key = JWK.asKey({
-        "crv": "P-256",
-        "x": "-f6jdHmkzQJXp2owZsPSNKVq-ZxrFrD0N8pXvUXGN2g",
-        "y": "Fce3qbxaO-KpplC7t2vufmcDmbaUVB9dT8bZR8MrI6w",
-        "kty": "EC",
-        "kid": "PGoXzs0NWaR_meKgTZLbEuDoSVTaFuyrbWI7V9dpjCg"
-    });
+    const recipient_key = generateSync("EC", "X25519")
 
     recipients = [
         {
@@ -36,7 +28,7 @@ test('example-encrypted-jwm-single-recipient', t => {
     options = {
       serialization: "general",
       enc: 'A256GCM',
-      //alg: 'ECDH-ES+A256KW'
+      alg: 'ECDH-ES+A256KW'
     }
     
     //Prepare JWM
@@ -46,7 +38,52 @@ test('example-encrypted-jwm-single-recipient', t => {
 
     t.is(protected_header.type, "JWM")
     t.is(protected_header.enc, "A256GCM")
-    //t.is(protected_header.alg, "ECDH-ES+A256KW")
+    t.is(protected_header.alg, "ECDH-ES+A256KW")
+
+    const decrypted = JWM.decrypt(jwm, recipients[0].key, { complete: true })
+  
+    t.deepEqual(decrypted.jwm, plaintext)
+
+
+  })
+
+  test('example-encrypted-jwm-single-recipient-sender', t => {
+    //Plaintext definition
+    const plaintext = {
+        id : "urn:uuid:ef5a7369-f0b9-4143-a49d-2b9c7ee51117",
+        type : "hello-world-message-type",
+        from : "urn:uuid:8abdf5fb-621e-4cf5-a595-071bc2c91d82", 
+        expiry : 1516239022,
+        time_stamp : 1516269022,
+        body : { message: "Hello world!" }
+    };
+
+    //Get recipient key
+
+    const recipient_key = generateSync("EC", "X25519");
+    const sender_key = generateSync("EC", "X25519");
+
+    recipients = [
+        {
+          'key': recipient_key,
+          'header': { kid: recipient_key.kid }
+        }
+    ]
+
+    options = {
+      serialization: "general",
+      enc: 'A256GCM',
+      alg: 'ECDH-ES+A256KW' //ECDH-1PU when supported
+    }
+    
+    //Prepare JWM
+    const jwm = JWM.encrypt(plaintext, recipients, sender_key, options)
+
+    protected_header = JSON.parse(base64url.decode(jwm.protected))
+
+    t.is(protected_header.type, "JWM")
+    t.is(protected_header.enc, "A256GCM")
+    t.is(protected_header.alg, "ECDH-ES+A256KW")
 
     const decrypted = JWM.decrypt(jwm, recipients[0].key, { complete: true })
   
