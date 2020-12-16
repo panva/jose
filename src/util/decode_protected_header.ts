fa29d68cfdf0922c7e4dac24eb50161d1eab28d4
Filip Skokan <panva.ip@gmail.com>
2020-12-16 14:47:54 +0100

    feat: added utility function for decoding token's protected header

---
+++ src/util/decode_protected_header.ts
@@ -0,0 +1,58 @@
+import { ok as assert } from 'assert'
+
+import { decode as base64url } from './base64url.js'
+import { decoder } from '../lib/buffer_utils.js'
+import isObject from '../lib/is_object.js'
+import type { JWSHeaderParameters, JWEHeaderParameters } from '../types.d'
+
+export type ProtectedHeaderParameters = JWSHeaderParameters & JWEHeaderParameters
+
+/**
+ * Decodes the Protected Header of a JWE/JWS/JWT token utilizing any encoding.
+ *
+ * @example
+ * ```
+ * // ESM import
+ * import decodeProtectedHeader from 'jose/util/decode_protected_header'
+ * ```
+ *
+ * @example
+ * ```
+ * // CJS import
+ * const { default: decodeProtectedHeader } = require('jose/util/decode_protected_header')
+ * ```
+ *
+ * @example
+ * ```
+ * // usage
+ * const protectedHeader = decodeProtectedHeader(token)
+ * console.log(protectedHeader)
+ * ```
+ *
+ * @param token JWE/JWS/JWT token in any encoding.
+ */
+export default function decodeProtectedHeader(token: string | object) {
+  let protectedB64u!: string
+
+  if (typeof token === 'string') {
+    const parts = token.split('.')
+    if (parts.length === 3 || parts.length === 5) {
+      ;[protectedB64u] = parts
+    }
+  } else if (typeof token === 'object' && token) {
+    if ('protected' in token) {
+      protectedB64u = (<{ protected: string }>token).protected
+    } else {
+      throw new TypeError('Token does not contain a Protected Header')
+    }
+  }
+
+  try {
+    assert(typeof protectedB64u === 'string' && protectedB64u)
+    const result = JSON.parse(decoder.decode(base64url(protectedB64u!)))
+    assert(isObject(result))
+    return <ProtectedHeaderParameters>result
+  } catch (err) {
+    throw new TypeError('Invalid Token or Protected Header formatting')
+  }
+}
