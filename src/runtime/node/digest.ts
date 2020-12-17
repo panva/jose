6abd2ef962c89597318799d3064d4f62b0ada60f
Filip Skokan <panva.ip@gmail.com>
2020-12-17 13:42:02 +0100

    lint: lint with upgraded deps

--- src/runtime/node/digest.ts
+++ src/runtime/node/digest.ts
@@ -4,7 +4,5 @@ import type { DigestFunction } from '../interfaces.d'
 const digest: DigestFunction = (
   algorithm: 'sha256' | 'sha384' | 'sha512',
   data: Uint8Array,
-): Uint8Array => {
-  return createHash(algorithm).update(data).digest()
-}
+): Uint8Array => createHash(algorithm).update(data).digest()
 export default digest
--- src/runtime/node/zlib.ts
+++ src/runtime/node/zlib.ts
@@ -6,10 +6,5 @@ import type { InflateFunction, DeflateFunction } from '../../types.d'
 const inflateRaw = promisify(inflateRawCb)
 const deflateRaw = promisify(deflateRawCb)
 
-export const inflate: InflateFunction = async (input: Uint8Array) => {
-  return inflateRaw(input)
-}
-
-export const deflate: DeflateFunction = async (input: Uint8Array) => {
-  return deflateRaw(input)
-}
+export const inflate: InflateFunction = (input: Uint8Array) => inflateRaw(input)
+export const deflate: DeflateFunction = (input: Uint8Array) => deflateRaw(input)
