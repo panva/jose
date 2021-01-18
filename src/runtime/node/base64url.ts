808f06cd08b10cf53343afb35802cc6e5b95ea20
Filip Skokan <panva.ip@gmail.com>
2021-01-18 13:22:46 +0100

    perf: use 'base64url' encoding when available in Node.js runtime

--- src/runtime/node/base64url.ts
+++ src/runtime/node/base64url.ts
@@ -1,12 +1,21 @@
 import { decoder } from '../../lib/buffer_utils.js'
 
-export const encode = (input: Uint8Array | string) =>
-  Buffer.from(input).toString('base64').replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
+let encodeImpl: (input: Uint8Array | string) => string
 
-export const decode = (input: Uint8Array | string) => {
+function normalize(input: string | Uint8Array) {
   let encoded = input
   if (encoded instanceof Uint8Array) {
     encoded = decoder.decode(encoded)
   }
-  return new Uint8Array(Buffer.from(encoded, 'base64'))
+  return encoded
 }
+
+if (Buffer.isEncoding('base64url')) {
+  encodeImpl = (input) => Buffer.from(input).toString(<BufferEncoding>'base64url')
+} else {
+  encodeImpl = (input) =>
+    Buffer.from(input).toString('base64').replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
+}
+
+export const encode = encodeImpl
+export const decode = (input: Uint8Array | string) => Buffer.from(normalize(input), 'base64')
