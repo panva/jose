6a9c6cadb34362cef583ed9473cadc4e141aabab
Filip Skokan <panva.ip@gmail.com>
2021-03-16 09:33:32 +0100

    ci: automate github releases

---
+++ tools/release-notes.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const { execSync } = require("child_process");
+
+execSync('git show HEAD -- CHANGELOG.md > CHANGELOG.diff');
+
+const tag = execSync('git tag --points-at HEAD').toString().trim();
+
+fs.writeFileSync('notes.md', fs
+  .readFileSync('CHANGELOG.diff')
+  .toString()
+  .split('\n')
+  .filter((line) => line.startsWith('+') && !line.startsWith('+++'))
+  .map((line) => line.substr(1))
+  .slice(3)
+  .join('\n'))
+
+execSync(`gh release create ${tag} -dF notes.md`)
