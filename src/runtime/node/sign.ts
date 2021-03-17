cf5074e7e1333728f7632ee6785cc52ef32711bf
Filip Skokan <panva.ip@gmail.com>
2021-03-17 14:11:15 +0100

    feat(node): use libuv threadpool to sign in node >= 15.12.0

--- src/runtime/node/sign.ts
+++ src/runtime/node/sign.ts
@@ -1,4 +1,6 @@
-import { sign as oneShotSign, createHmac, KeyObject } from 'crypto'
+import * as crypto from 'crypto'
+import { promisify } from 'util'
+
 import type { KeyLike } from '../../types.d'
 import type { SignFunction } from '../interfaces.d'
 import nodeDigest from './dsa_digest.js'
@@ -7,8 +9,14 @@ import nodeKey from './node_key.js'
 import getSecretKey from './secret_key.js'
 import { isCryptoKey, getKeyObject } from './webcrypto.js'
 
+let oneShotSign = crypto.sign
+if (oneShotSign.length > 3) {
+  // @ts-expect-error
+  oneShotSign = promisify(oneShotSign)
+}
+
 const sign: SignFunction = async (alg, key: KeyLike, data) => {
-  let keyObject: KeyObject
+  let keyObject: crypto.KeyObject
   if (key instanceof Uint8Array) {
     if (!alg.startsWith('HS')) {
       throw new TypeError('symmetric keys are only applicable for HMAC-based JWA algorithms')
@@ -25,7 +33,7 @@ const sign: SignFunction = async (alg, key: KeyLike, data) => {
     if (!keyObject.symmetricKeySize || keyObject.symmetricKeySize << 3 < bitlen) {
       throw new TypeError(`${alg} requires symmetric keys to be ${bitlen} bits or larger`)
     }
-    const hmac = createHmac(hmacDigest(alg), keyObject)
+    const hmac = crypto.createHmac(hmacDigest(alg), keyObject)
     hmac.update(data)
     return hmac.digest()
   }
