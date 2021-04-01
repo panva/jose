54343596b37b0a3651db23db36af3815b439fddd
Filip Skokan <panva.ip@gmail.com>
2021-04-01 14:15:06 +0200

    test: add X25519 and X448 ECDH test vectors

--- test/unit/encrypt_key_management.test.mjs
+++ test/unit/encrypt_key_management.test.mjs
@@ -1,11 +1,23 @@
 import test from 'ava';
 
-const root = !('WEBCRYPTO' in process.env) ? '#dist' : '#dist/webcrypto';
+let root;
+let keyRoot;
+
+if ('WEBCRYPTO' in process.env) {
+  root = keyRoot = '#dist/webcrypto';
+} else if ('CRYPTOKEY' in process.env) {
+  root = '#dist';
+  keyRoot = '#dist/webcrypto';
+} else {
+  root = keyRoot = '#dist';
+}
+
 Promise.all([
   import(`${root}/lib/encrypt_key_management`),
-  import(`${root}/util/generate_key_pair`),
+  import(`${keyRoot}/util/generate_key_pair`),
+  import(`${keyRoot}/jwk/parse`),
 ]).then(
-  ([{ default: encryptKeyManagement }, { default: generateKeyPair }]) => {
+  ([{ default: encryptKeyManagement }, { default: generateKeyPair }, { default: parseJwk }]) => {
     test('lib/encrypt_key_management.ts', async (t) => {
       await t.throwsAsync(encryptKeyManagement('foo'), {
         code: 'ERR_JOSE_NOT_SUPPORTED',
@@ -15,7 +27,7 @@ Promise.all([
 
     function conditional({ webcrypto = 1, electron = 1 } = {}, ...args) {
       let run = test;
-      if (!webcrypto && 'WEBCRYPTO' in process.env) {
+      if ((!webcrypto && 'WEBCRYPTO' in process.env) || 'CRYPTOKEY' in process.env) {
         run = run.failing;
       }
 
@@ -36,6 +48,84 @@ Promise.all([
         });
       },
     );
+
+    conditional({ webcrypto: 0 })('RFC8037 - A.6. ECDH-ES with X25519', async (t) => {
+      const [publicKey, ephemeral] = await Promise.all([
+        parseJwk(
+          {
+            kty: 'OKP',
+            crv: 'X25519',
+            x: '3p7bfXt9wbTTW2HC7OQ1Nz-DQ8hbeGdNrfx-FG-IK08',
+          },
+          'ECDH-ES',
+        ),
+        parseJwk(
+          {
+            kty: 'OKP',
+            crv: 'X25519',
+            x: 'hSDwCYkwp1R0i33ctD73Wg2_Og0mOBr066SpjqqbTmo',
+            d: 'dwdtCnMYpX08FsFyUbJmRd9ML4frwJkqsXf7pR25LCo',
+          },
+          'ECDH-ES',
+        ),
+      ]);
+
+      const keyManagement = await encryptKeyManagement('ECDH-ES', 'A256GCM', publicKey, undefined, {
+        epk: ephemeral,
+      });
+
+      t.deepEqual(keyManagement, {
+        encryptedKey: undefined,
+        parameters: {
+          epk: {
+            x: 'hSDwCYkwp1R0i33ctD73Wg2_Og0mOBr066SpjqqbTmo',
+            y: undefined,
+            crv: 'X25519',
+            kty: 'OKP',
+          },
+        },
+        cek: Buffer.from('d3f1a933aa331e5d3204beab09cddb96dbdaba47a555530b70fa4af7187c6edd', 'hex'),
+      });
+    });
+
+    conditional({ webcrypto: 0, electron: 0 })('RFC8037 - A.6. ECDH-ES with X448', async (t) => {
+      const [publicKey, ephemeral] = await Promise.all([
+        parseJwk(
+          {
+            kty: 'OKP',
+            crv: 'X448',
+            x: 'PreoKbDNIPW8_AtZm2_sz22kYnEHvbDU80W0MCfYuXL8PjT7QjKhPKcG3LV67D2uB73BxnvzNgk',
+          },
+          'ECDH-ES',
+        ),
+        parseJwk(
+          {
+            kty: 'OKP',
+            crv: 'X448',
+            x: 'mwj3zDG34-Z9ItWuoSEHSic70rg94Jxj-qc9LCLF2bvINmRyQdlT1AxbEtqIEg1TF3-A5TLEH6A',
+            d: 'mo9JJdFRn1d1z0awS1gA1O6e6LrovFVl1JjCjdnJuvV0qUGXRIlzkQBjgqbxJ6sdmsLYwKWYcms',
+          },
+          'ECDH-ES',
+        ),
+      ]);
+
+      const keyManagement = await encryptKeyManagement('ECDH-ES', 'A256GCM', publicKey, undefined, {
+        epk: ephemeral,
+      });
+
+      t.deepEqual(keyManagement, {
+        encryptedKey: undefined,
+        parameters: {
+          epk: {
+            x: 'mwj3zDG34-Z9ItWuoSEHSic70rg94Jxj-qc9LCLF2bvINmRyQdlT1AxbEtqIEg1TF3-A5TLEH6A',
+            y: undefined,
+            crv: 'X448',
+            kty: 'OKP',
+          },
+        },
+        cek: Buffer.from('5171b73b1b4a7ef896ef4cb1775da5db6256dc885f260a0ae3bbe0632420bf7c', 'hex'),
+      });
+    });
   },
   (err) => {
     test('failed to import', (t) => {
