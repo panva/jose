93b6c6cb5ab9cc471f278b02cc293909ee6b9c56
Cody Ebberson <cody@ebberson.com>
2021-06-21 14:48:42 -0700

    Export type JWTVerifyResult

--- src/jwt/verify.ts
+++ src/jwt/verify.ts
@@ -71,4 +71,4 @@ async function jwtVerify(
 
 export { jwtVerify }
 export default jwtVerify
-export type { KeyLike, JWTPayload, JWTVerifyOptions, JWSHeaderParameters, GetKeyFunction }
+export type { KeyLike, JWTPayload, JWTVerifyOptions, JWSHeaderParameters, GetKeyFunction, JWTVerifyResult }
