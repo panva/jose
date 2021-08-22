099f7f5887e6cd2512e0f52e2912e9c75a2e4ed0
Filip Skokan <panva.ip@gmail.com>
2021-08-22 17:52:56 +0200

    refactor(webcrypto): update missing zlib descriptions

--- src/runtime/browser/zlib.ts
+++ src/runtime/browser/zlib.ts
@@ -3,11 +3,11 @@ import type { InflateFunction, DeflateFunction } from '../../types.d'
 
 export const inflate: InflateFunction = async () => {
   throw new JOSENotSupported(
-    'JWE "zip" (Compression Algorithm) Header Parameter is not supported by your javascript runtime. You need to use the `inflateRaw` decrypt option to provide Inflate Raw implementation, e.g. using the "pako" module.',
+    'JWE "zip" (Compression Algorithm) Header Parameter is not supported by your javascript runtime. You need to use the `inflateRaw` decrypt option to provide Inflate Raw implementation.',
   )
 }
 export const deflate: DeflateFunction = async () => {
   throw new JOSENotSupported(
-    'JWE "zip" (Compression Algorithm) Header Parameter is not supported by your javascript runtime.',
+    'JWE "zip" (Compression Algorithm) Header Parameter is not supported by your javascript runtime. You need to use the `deflateRaw` encrypt option to provide Deflate Raw implementation.',
   )
 }
