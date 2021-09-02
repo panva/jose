d1b6f14b9790e7436f2586b52754104981d1bebe
Owen Delahoy <owen.delahoy@gmail.com>
2021-09-03 02:14:05 +0900

    refactor: create abort signal

--- src/runtime/browser/fetch_jwks.ts
+++ src/runtime/browser/fetch_jwks.ts
@@ -2,24 +2,25 @@ import type { FetchFunction } from '../interfaces.d'
 import { JOSEError } from '../../util/errors.js'
 import globalThis from './global.js'
 
+function createAbortSignal(ms?: number) {
+  if (typeof (AbortController as unknown) !== 'function') { return }
+  const controller = new AbortController()
+  setTimeout(() => controller.abort(), ms)
+  return controller.signal
+}
 
 const isCloudflareWorker = ("mode" in new Request(""))
 
 const fetchJwks: FetchFunction = async (url: URL, timeout: number) => {
-  let controller!: AbortController
-  if (typeof AbortController === 'function') {
-    controller = new AbortController()
-    setTimeout(() => controller.abort(), timeout)
-  }
-
+  const signal = createAbortSignal(timeout)
   const referrerPolicy = !isCloudflareWorker ? 'no-referrer' : undefined
   const credentials =    !isCloudflareWorker ? 'omit' : undefined
   const mode =           !isCloudflareWorker ? 'cors' : undefined
 
   const response = await globalThis.fetch(url.href, {
-    signal: controller ? controller.signal : undefined,
     redirect: 'manual',
     method: 'GET',
+    signal,
     referrerPolicy,
     credentials,
     mode,
