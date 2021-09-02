2231e49012fbf3a8c2fbea7588e956af0605c3ea
Filip Skokan <panva.ip@gmail.com>
2021-09-02 17:00:30 +0000

    chore: cleanup after publish

--- dist/types/jws/compact/verify.d.ts
+++
@@ -1,7 +0,0 @@
-import type { CompactVerifyResult, FlattenedJWSInput, GetKeyFunction, JWSHeaderParameters, KeyLike, VerifyOptions } from '../../types';
-export interface CompactVerifyGetKey extends GetKeyFunction<JWSHeaderParameters, FlattenedJWSInput> {
-}
-declare function compactVerify(jws: string | Uint8Array, key: KeyLike | CompactVerifyGetKey, options?: VerifyOptions): Promise<CompactVerifyResult>;
-export { compactVerify };
-export default compactVerify;
-export type { KeyLike, VerifyOptions, CompactVerifyResult };
--- dist/types/util/random.d.ts
+++
@@ -1,6 +0,0 @@
-interface GetRandomValuesFunction {
-    (array: Uint8Array): Uint8Array;
-}
-declare const random: GetRandomValuesFunction;
-export { random };
-export default random;
