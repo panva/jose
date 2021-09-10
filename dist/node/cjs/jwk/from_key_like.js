a2b0eec71470706bf38368854d896e073a50897d
Filip Skokan <panva.ip@gmail.com>
2021-09-10 09:31:19 +0000

    chore: cleanup after publish

--- dist/node/cjs/jwk/from_key_like.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fromKeyLike = void 0;
-const key_to_jwk_js_1 = require("../runtime/key_to_jwk.js");
-async function fromKeyLike(key) {
-    return (0, key_to_jwk_js_1.default)(key);
-}
-exports.fromKeyLike = fromKeyLike;
-exports.default = fromKeyLike;
--- dist/types/jwk/from_key_like.d.ts
+++
@@ -1,5 +0,0 @@
-import type { JWK, KeyLike } from '../types';
-declare function fromKeyLike(key: KeyLike): Promise<JWK>;
-export { fromKeyLike };
-export default fromKeyLike;
-export type { KeyLike, JWK };
