aa32f78e1d48adb4cf4f3a364f8718eea95a9f65
Filip Skokan <panva.ip@gmail.com>
2021-09-26 11:07:37 +0000

    chore: cleanup after publish

--- dist/deno/util/random.ts
+++
@@ -1,9 +0,0 @@
-import runtimeRandom from '../runtime/random.ts'
-
-interface GetRandomValuesFunction {
-  (array: Uint8Array): Uint8Array
-}
-
-const random: GetRandomValuesFunction = runtimeRandom
-export { random }
-export default random
--- dist/types/key/export.d.ts
+++
@@ -1,5 +0,0 @@
-import type { JWK, KeyLike } from '../types';
-export declare function exportSPKI(key: Exclude<KeyLike, Uint8Array>): Promise<string>;
-export declare function exportPKCS8(key: Exclude<KeyLike, Uint8Array>): Promise<string>;
-export declare function exportJWK(key: KeyLike): Promise<JWK>;
-export type { KeyLike, JWK };
