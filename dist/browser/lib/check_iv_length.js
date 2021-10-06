7799576c3355f147a751ba980235423bb8575246
Filip Skokan <panva.ip@gmail.com>
2021-10-06 14:39:19 +0000

    chore: cleanup after publish

--- dist/browser/lib/check_iv_length.js
+++
@@ -1,8 +0,0 @@
-import { JWEInvalid } from '../util/errors.js';
-import { bitLengths } from './iv.js';
-const checkIvLength = (enc, iv) => {
-    if (iv.length << 3 !== bitLengths.get(enc)) {
-        throw new JWEInvalid('Invalid Initialization Vector length');
-    }
-};
-export default checkIvLength;
