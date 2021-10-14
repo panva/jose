0237115b1ab590b1f9cce0b75130b88de726830f
Filip Skokan <panva.ip@gmail.com>
2021-10-14 10:46:00 +0000

    chore: cleanup after publish

--- dist/browser/runtime/webcrypto.js
+++
@@ -1,8 +0,0 @@
-import globalThis from './global.js';
-export default globalThis.crypto;
-export function isCryptoKey(key) {
-    if (typeof globalThis.CryptoKey === 'undefined') {
-        return false;
-    }
-    return key != null && key instanceof globalThis.CryptoKey;
-}
