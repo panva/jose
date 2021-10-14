b93548f852cd80197b2954ce456dea11ab314e45
Filip Skokan <panva.ip@gmail.com>
2021-10-14 16:44:49 +0000

    chore: cleanup after publish

--- dist/types/util/decode_protected_header.d.ts
+++
@@ -1,3 +0,0 @@
-import type { JWSHeaderParameters, JWEHeaderParameters } from '../types';
-export declare type ProtectedHeaderParameters = JWSHeaderParameters & JWEHeaderParameters;
-export declare function decodeProtectedHeader(token: string | object): ProtectedHeaderParameters;
