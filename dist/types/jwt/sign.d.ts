244e2f9db7c1fd6e7ce80f6a7cc54a2e34118b8e
Filip Skokan <panva.ip@gmail.com>
2021-10-16 08:21:22 +0000

    chore: cleanup after publish

--- dist/types/jwt/sign.d.ts
+++
@@ -1,7 +0,0 @@
-import type { JWSHeaderParameters, KeyLike, SignOptions } from '../types';
-import { ProduceJWT } from './produce';
-export declare class SignJWT extends ProduceJWT {
-    private _protectedHeader;
-    setProtectedHeader(protectedHeader: JWSHeaderParameters): this;
-    sign(key: KeyLike | Uint8Array, options?: SignOptions): Promise<string>;
-}
