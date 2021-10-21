27cc2212a1f253a8a2dc6b39ac498b4aeb900c6e
Filip Skokan <panva.ip@gmail.com>
2021-10-21 08:27:27 +0000

    chore: cleanup after publish

--- dist/types/jwe/flattened/decrypt.d.ts
+++
@@ -1,5 +0,0 @@
-import type { FlattenedDecryptResult, KeyLike, FlattenedJWE, JWEHeaderParameters, DecryptOptions, GetKeyFunction, ResolvedKey } from '../../types';
-export interface FlattenedDecryptGetKey extends GetKeyFunction<JWEHeaderParameters | undefined, FlattenedJWE> {
-}
-export declare function flattenedDecrypt(jwe: FlattenedJWE, key: KeyLike | Uint8Array, options?: DecryptOptions): Promise<FlattenedDecryptResult>;
-export declare function flattenedDecrypt(jwe: FlattenedJWE, getKey: FlattenedDecryptGetKey, options?: DecryptOptions): Promise<FlattenedDecryptResult & ResolvedKey>;
