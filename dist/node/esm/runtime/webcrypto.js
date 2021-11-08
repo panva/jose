54fc0e3a6056fbc764ac34351f64cadc97578e48
Filip Skokan <panva.ip@gmail.com>
2021-11-08 21:49:11 +0000

    chore: cleanup after publish

--- dist/node/esm/runtime/webcrypto.js
+++
@@ -1,8 +0,0 @@
-import * as crypto from 'crypto';
-import * as util from 'util';
-const webcrypto = crypto.webcrypto;
-export default webcrypto;
-export const isCryptoKey = util.types.isCryptoKey
-    ? (obj) => util.types.isCryptoKey(obj)
-    :
-        (obj) => false;
--- dist/types/util/base64url.d.ts
+++
@@ -1,9 +0,0 @@
-interface Base64UrlEncode {
-    (input: Uint8Array | string): string;
-}
-interface Base64UrlDecode {
-    (input: Uint8Array | string): Uint8Array;
-}
-export declare const encode: Base64UrlEncode;
-export declare const decode: Base64UrlDecode;
-export {};
