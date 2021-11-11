808877ae882e20b440da226727bf75a78390a6ab
Filip Skokan <panva.ip@gmail.com>
2021-11-11 14:29:25 +0100

    refactor: src/runtime/node/hmac_digest.ts

--- src/runtime/node/hmac_digest.ts
+++ src/runtime/node/hmac_digest.ts
@@ -4,13 +4,10 @@ export default function hmacDigest(alg: string) {
   switch (alg) {
     case 'HS256':
       return 'sha256'
-
     case 'HS384':
       return 'sha384'
-
     case 'HS512':
       return 'sha512'
-
     default:
       throw new JOSENotSupported(
         `alg ${alg} is not supported either by JOSE or your javascript runtime`,
