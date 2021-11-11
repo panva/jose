9c66d0b8187bab446d1590c52527d281a866bd92
Filip Skokan <panva.ip@gmail.com>
2021-11-11 22:39:41 +0000

    chore: cleanup after publish

--- dist/types/jwk/embedded.d.ts
+++
@@ -1,2 +0,0 @@
-import type { FlattenedJWSInput, JWSHeaderParameters } from '../types';
-export declare function EmbeddedJWK(protectedHeader: JWSHeaderParameters, token: FlattenedJWSInput): Promise<import("../types.d").KeyLike>;
