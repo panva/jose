7645ff00f4fb9534d3a520587e9c690a48e6e53d
Filip Skokan <panva.ip@gmail.com>
2021-11-11 16:20:30 +0000

    chore: cleanup after publish

--- dist/types/key/generate_secret.d.ts
+++
@@ -1,5 +0,0 @@
-import type { KeyLike } from '../types';
-export interface GenerateSecretOptions {
-    extractable?: boolean;
-}
-export declare function generateSecret(alg: string, options?: GenerateSecretOptions): Promise<KeyLike | Uint8Array>;
