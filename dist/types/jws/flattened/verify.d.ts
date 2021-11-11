b5e707c6d5c53a652b2ac514e6bcdc03e83d1ed7
Filip Skokan <panva.ip@gmail.com>
2021-11-11 22:56:04 +0100

    chore: cleanup after publish

--- dist/types/jws/flattened/verify.d.ts
+++
@@ -1,5 +0,0 @@
-import type { FlattenedVerifyResult, KeyLike, FlattenedJWSInput, JWSHeaderParameters, VerifyOptions, GetKeyFunction, ResolvedKey } from '../../types';
-export interface FlattenedVerifyGetKey extends GetKeyFunction<JWSHeaderParameters | undefined, FlattenedJWSInput> {
-}
-export declare function flattenedVerify(jws: FlattenedJWSInput, key: KeyLike | Uint8Array, options?: VerifyOptions): Promise<FlattenedVerifyResult>;
-export declare function flattenedVerify(jws: FlattenedJWSInput, getKey: FlattenedVerifyGetKey, options?: VerifyOptions): Promise<FlattenedVerifyResult & ResolvedKey>;
