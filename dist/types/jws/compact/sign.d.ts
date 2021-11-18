0e5c29f47677980d061525e4be22be3444b3491a
Filip Skokan <panva.ip@gmail.com>
2021-11-18 07:14:03 +0000

    chore: cleanup after publish

--- dist/types/jws/compact/sign.d.ts
+++
@@ -1,7 +0,0 @@
-import type { CompactJWSHeaderParameters, KeyLike, SignOptions } from '../../types';
-export declare class CompactSign {
-    private _flattened;
-    constructor(payload: Uint8Array);
-    setProtectedHeader(protectedHeader: CompactJWSHeaderParameters): this;
-    sign(key: KeyLike | Uint8Array, options?: SignOptions): Promise<string>;
-}
