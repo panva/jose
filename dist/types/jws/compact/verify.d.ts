69b1b32fd2440ee6026d24c644f9ac6afb240008
Filip Skokan <panva.ip@gmail.com>
2022-01-22 17:29:44 +0000

    chore: cleanup after publish

--- dist/types/jws/compact/verify.d.ts
+++
@@ -1,5 +0,0 @@
-import type { CompactVerifyResult, FlattenedJWSInput, GetKeyFunction, CompactJWSHeaderParameters, KeyLike, VerifyOptions, ResolvedKey } from '../../types';
-export interface CompactVerifyGetKey extends GetKeyFunction<CompactJWSHeaderParameters, FlattenedJWSInput> {
-}
-export declare function compactVerify(jws: string | Uint8Array, key: KeyLike | Uint8Array, options?: VerifyOptions): Promise<CompactVerifyResult>;
-export declare function compactVerify(jws: string | Uint8Array, getKey: CompactVerifyGetKey, options?: VerifyOptions): Promise<CompactVerifyResult & ResolvedKey>;
