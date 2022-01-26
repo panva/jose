73c9b3df4c353b997823890cbf7de07699a4cbc5
Filip Skokan <panva.ip@gmail.com>
2022-01-26 14:31:36 +0100

    refactor: src/runtime/node/base64url.ts

--- src/runtime/node/base64url.ts
+++ src/runtime/node/base64url.ts
@@ -1,7 +1,7 @@
 import { Buffer } from 'buffer'
 import { decoder } from '../../lib/buffer_utils.js'
 
-let encodeImpl: (input: Uint8Array | string) => string
+let encode: (input: Uint8Array | string) => string
 
 function normalize(input: string | Uint8Array) {
   let encoded = input
@@ -12,13 +12,13 @@ function normalize(input: string | Uint8Array) {
 }
 
 if (Buffer.isEncoding('base64url')) {
-  encodeImpl = (input) => Buffer.from(input).toString('base64url')
+  encode = (input) => Buffer.from(input).toString('base64url')
 } else {
-  encodeImpl = (input) =>
+  encode = (input) =>
     Buffer.from(input).toString('base64').replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
 }
 
 export const decodeBase64 = (input: string) => Buffer.from(input, 'base64')
 export const encodeBase64 = (input: Uint8Array | string) => Buffer.from(input).toString('base64')
-export const encode = encodeImpl
+export { encode }
 export const decode = (input: Uint8Array | string) => Buffer.from(normalize(input), 'base64')
