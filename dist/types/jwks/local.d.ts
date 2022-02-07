8b997bb3305d8bbc4e9854d073e8b765c0cb92dd
Filip Skokan <panva.ip@gmail.com>
2022-02-07 13:14:30 +0000

    chore: cleanup after publish

--- dist/types/jwks/local.d.ts
+++
@@ -1,9 +0,0 @@
-import type { KeyLike, JWSHeaderParameters, JSONWebKeySet, FlattenedJWSInput, GetKeyFunction } from '../types';
-export declare function isJWKSLike(jwks: unknown): jwks is JSONWebKeySet;
-export declare class LocalJWKSet {
-    protected _jwks?: JSONWebKeySet;
-    private _cached;
-    constructor(jwks: unknown);
-    getKey(protectedHeader: JWSHeaderParameters, token: FlattenedJWSInput): Promise<KeyLike>;
-}
-export declare function createLocalJWKSet(jwks: JSONWebKeySet): GetKeyFunction<JWSHeaderParameters, FlattenedJWSInput>;
