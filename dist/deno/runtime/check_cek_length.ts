8d016fdb5825dc0610707b142e898981bf1e5a47
Filip Skokan <panva.ip@gmail.com>
2022-02-22 10:36:26 +0000

    chore: cleanup after publish

--- dist/deno/runtime/check_cek_length.ts
+++
@@ -1,9 +0,0 @@
-import { JWEInvalid } from '../util/errors.ts'
-
-const checkCekLength = (cek: Uint8Array, expected: number) => {
-  if (cek.length << 3 !== expected) {
-    throw new JWEInvalid('Invalid Content Encryption Key length')
-  }
-}
-
-export default checkCekLength
--- dist/types/jwt/sign.d.ts
+++
@@ -1,7 +0,0 @@
-import type { JWTHeaderParameters, KeyLike, SignOptions } from '../types';
-import { ProduceJWT } from './produce';
-export declare class SignJWT extends ProduceJWT {
-    private _protectedHeader;
-    setProtectedHeader(protectedHeader: JWTHeaderParameters): this;
-    sign(key: KeyLike | Uint8Array, options?: SignOptions): Promise<string>;
-}
