7378d4d00012b55af46d4f0b91a541d9b6731cac
Filip Skokan <panva.ip@gmail.com>
2022-03-05 10:26:21 +0000

    chore: cleanup after publish

--- dist/types/jwe/compact/decrypt.d.ts
+++
@@ -1,5 +0,0 @@
-import type { KeyLike, DecryptOptions, CompactJWEHeaderParameters, GetKeyFunction, FlattenedJWE, CompactDecryptResult, ResolvedKey } from '../../types';
-export interface CompactDecryptGetKey extends GetKeyFunction<CompactJWEHeaderParameters, FlattenedJWE> {
-}
-export declare function compactDecrypt(jwe: string | Uint8Array, key: KeyLike | Uint8Array, options?: DecryptOptions): Promise<CompactDecryptResult>;
-export declare function compactDecrypt(jwe: string | Uint8Array, getKey: CompactDecryptGetKey, options?: DecryptOptions): Promise<CompactDecryptResult & ResolvedKey>;
