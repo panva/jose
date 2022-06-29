3281e688b9449e6fed0999e3cf30a680322646e5
Filip Skokan <panva.ip@gmail.com>
2022-06-29 00:28:55 +0000

    chore: cleanup after publish

--- dist/browser/runtime/env.js
+++
@@ -1,3 +0,0 @@
-export function isCloudflareWorkers() {
-    return typeof WebSocketPair === 'function';
-}
--- dist/node/cjs/runtime/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isCloudflareWorkers = void 0;
-function isCloudflareWorkers() {
-    return false;
-}
-exports.isCloudflareWorkers = isCloudflareWorkers;
