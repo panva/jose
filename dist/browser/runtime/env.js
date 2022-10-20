ca3a23d7512a17c0697b910884efeffda70ff80c
Filip Skokan <panva.ip@gmail.com>
2022-10-20 16:58:39 +0000

    chore: cleanup after publish

--- dist/browser/runtime/env.js
+++
@@ -1,5 +0,0 @@
-export function isCloudflareWorkers() {
-    return (typeof WebSocketPair !== 'undefined' ||
-        (typeof navigator !== 'undefined' && navigator.userAgent === 'Cloudflare-Workers') ||
-        (typeof EdgeRuntime !== 'undefined' && EdgeRuntime === 'vercel'));
-}
