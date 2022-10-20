d683e8656f61630460fae675db5adda66b304d78
Filip Skokan <panva.ip@gmail.com>
2022-10-20 13:21:12 +0000

    chore: cleanup after publish

--- dist/node/cjs/lib/format_pem.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (b64, descriptor) => {
-    const newlined = (b64.match(/.{1,64}/g) || []).join('\n');
-    return `-----BEGIN ${descriptor}-----\n${newlined}\n-----END ${descriptor}-----`;
-};
