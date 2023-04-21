c4be36bcdce4f385e541b2d7a62420b09be2c6ec
Filip Skokan <panva.ip@gmail.com>
2023-04-21 23:08:26 +0200

    build: add default title to gh release

--- tools/release-notes.js
+++ tools/release-notes.js
@@ -17,4 +17,4 @@ fs.writeFileSync(
     .join('\n'),
 )
 
-execSync(`gh release create ${tag} -F notes.md --discussion-category Releases`)
+execSync(`gh release create ${tag} -F notes.md --title ${tag} --discussion-category Releases`)
