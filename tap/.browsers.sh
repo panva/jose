f57c197846b6bc5a4d10b8f8bc202d5cba964fea
Filip Skokan <panva.ip@gmail.com>
2023-08-27 17:44:37 +0200

    test: run webkit in github actions safari

--- tap/.browsers.sh
+++ tap/.browsers.sh
@@ -12,15 +12,6 @@ SSL=""
 
 if [[ -z $CI ]]; then
   BROWSER="chrome:headless"
-else
-  if [[ "$BROWSER" == "browserstack"* ]]; then
-    if [[ "$BROWSER" != "browserstack:android"* ]]; then
-      HOSTNAME="jose.panva.me"
-      SSL="key=./letsencrypt/config/live/jose.panva.me/privkey.pem;cert=./letsencrypt/config/live/jose.panva.me/cert.pem;rejectUnauthorized=true;"
-    fi
-    BROWSER=$(NODE_PATH=$(npm root -g) node ./tap/browserstack.cjs $BROWSER)
-    echo "Using $BROWSER"
-  fi
 fi
 
 testcafe "$BROWSER" --skip-js-errors --ssl "$SSL" --hostname "$HOSTNAME" tap/.browser.ts
