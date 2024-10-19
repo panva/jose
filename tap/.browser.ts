e2f737dc9b191941a17a1da1a05d5dda0983952d
Filip Skokan <panva.ip@gmail.com>
2024-10-19 13:23:43 +0200

    test: temporary workaround chrome 130 and testcafe

--- tap/.browser.ts
+++ tap/.browser.ts
@@ -1,4 +1,4 @@
-import { t, Selector } from 'testcafe'
+import { t, ClientFunction } from 'testcafe'
 import * as fs from 'node:fs'
 
 const script = fs.readFileSync('./tap/run-browser.js', { encoding: 'utf-8' })
@@ -6,11 +6,23 @@ const script = fs.readFileSync('./tap/run-browser.js', { encoding: 'utf-8' })
 fixture('test suite').page('https://important-clam-66.deno.dev')
 
 test('passes tests', async (user) => {
-  const scriptTag = Selector('script')
+  await ClientFunction(
+    () => {
+      function escapeHTML(str: string) {
+        return str
+          .replace(/&/g, '&amp;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+          .replace(/"/g, '&quot;')
+          .replace(/'/g, '&#39;')
+      }
+      // @ts-expect-error
+      document.getElementById('js').innerHTML = escapeHTML(innerHTML)
+      document.querySelector('form')?.submit()
+    },
+    { dependencies: { innerHTML: script } },
+  )()
 
-  await user.typeText('#js', script, { paste: true }).click('[type=submit]')
-
-  await scriptTag()
   let i = 0
   const interval = setInterval(() => {
     t.getBrowserConsoleMessages().then(({ log: messages }) => {
