d4e396091e5f9c509f7fca0b5b3c548e5bb22b8a
Filip Skokan <panva.ip@gmail.com>
2024-12-04 14:24:16 +0100

    chore: cleanup test flags and tested Node.js versions

--- .node_flags.sh
+++ .node_flags.sh
@@ -3,7 +3,3 @@ echo "Using Node.js $(node --version)"
 node -e 'process.exit(parseInt(process.versions.node, 10))' &> /dev/null
 NODE_VERSION=$?
 export NODE_OPTIONS='--no-warnings --enable-source-maps --import=tsx/esm'
-
-if [[ $NODE_VERSION -eq 18 ]]; then
-  export NODE_OPTIONS+=' --experimental-global-webcrypto'
-fi
